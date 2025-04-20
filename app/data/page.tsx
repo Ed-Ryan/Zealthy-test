da9b0a689a9f5127eefbbfd34fb3a235240e814a
Ed-Ryan <120978416+Ed-Ryan@users.noreply.github.com>
2025-04-20 16:41:22 -0400

    Update page.tsx

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -3,7 +3,8 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import prisma from '@/lib/prisma';
+//import prisma from '@/lib/prisma';
+import prisma from '../../lib/prisma';
 
 
 interface User {
