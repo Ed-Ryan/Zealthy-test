999fac3c2fd9b9371784a1a14a56fee5b40a54fe
Ed-Ryan <120978416+Ed-Ryan@users.noreply.github.com>
2025-04-20 16:26:29 -0400

    Update route.ts

--- app/api/get-users/route.ts
+++ app/api/get-users/route.ts
@@ -1,6 +1,6 @@
 // app/api/get-users/route.ts
 import { NextResponse } from 'next/server';
-import prisma from '../../../lib/prisma'; // double check the path
+import prisma from '../../lib/prisma'; // double check the path
 
 export async function GET() {
   return NextResponse.json({
